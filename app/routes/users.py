50f801a71f428a15a975d5963f8a71ec493b6852
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-02-16 01:44:08 +0530

    create and get all users create -- validation or username and email already exist

---
+++ app/routes/users.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+from ..db import get_db
+from .. import schemas, models, utilities
+from typing import List
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+@router.get("/", response_model=List[schemas.UserResponse],status_code=status.HTTP_200_OK)
+def get_user(search : str ="", limit: int=10, skip: int = 0, db : Session = Depends(get_db)):
+    all_users = db.query(models.Users).filter(models.Users.name.contains(search)).limit(limit=limit).offset(offset=skip).all()
+    return all_users
+
+@router.post("/",response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    Hashed_password = utilities.create_hash_password(user.password)
+    user.password = Hashed_password
+    new_user = models.Users(**user.model_dump(exclude_unset=True))
+    db.add(new_user)
+    try: 
+        db.commit()
+    except IntegrityError as e:
+        db.rollback()
+        error_msg = str(e.orig)
+        if "users_username_key" in error_msg:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Username already exist")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already Exist")
+    db.refresh(new_user)
+    return new_user
