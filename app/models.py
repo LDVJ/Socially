ab0b931648f997513ee062b7b5043364012dcd97
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-02-21 20:28:17 +0530

    follow system

--- app/models.py
+++ app/models.py
@@ -41,3 +41,16 @@ class PostLikes(Base):
         UniqueConstraint("post_id","user_id", name="user_post_like"),
     )
 
+class FollowList(Base):
+    __tablename__ = "follow_list"
+
+    id : Mapped[int] = mapped_column(primary_key=True, nullable=False)
+
+    followed_uid : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    follower_uid : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    __table_args__ = (
+        UniqueConstraint("followed_uid", "follower_uid", name="followers_unique"),
+    )
--- app/schemas.py
+++ app/schemas.py
@@ -20,11 +20,11 @@ class PostBase(BaseModel):
         "from_attributes" : True
     }
 
-#input schema for user
-
+#create schema for user
 class UserCreate(UserBase):
     password : str
 
+# update user 
 class UpdateUser(BaseModel):
     id: int
     username : str | None = None
@@ -34,6 +34,7 @@ class UpdateUser(BaseModel):
         "from_attributes":True
     }
 
+#update post
 class UpdatePost(BaseModel):
     id: int
     title: str | None = None
@@ -44,37 +45,45 @@ class UpdatePost(BaseModel):
         "from_attributes":True
     }
 
+# user response model
 class UserResponse(UserBase): #independant schema 
     id : int
     created_at : datetime
 
+# reponse of post along with user detail of who created it
 class PostUserResponse(PostBase):
     id: int
     created_at : datetime
     updated_at : datetime | None = None
 
+# reponse of post details along with like countt
 class PostLikeCount(BaseModel):
     Posts: PostUserResponse
     like_count : int
 
+# response of the posts along with the users detail who liked the post
 class PostUserList(BaseModel):
     Posts: PostUserResponse
     like_count : int
     Users: List[UpdateUser]
 
+# post response from the along with the user details based on relation of  models
 class PostResponse(PostUserResponse): #independent schema
     owner_id : int
     owner : UserResponse
+
 
 class UserPostResponse(UserBase):
     id: int
     created_at: datetime
     user_posts: List[PostUserResponse]
 
-class ValidUser(BaseModel):
-    email : EmailStr
-    password : str
+class PostLikes(BaseModel):
+    id: int
+    post_id : int
+    user_id : int
 
+# bearer token schemas 
 class TokenData(BaseModel):
     id : int
 
@@ -82,7 +91,8 @@ class ValidToken(BaseModel):
     access_token : str
     token_type : str
 
-class PostLikes(BaseModel):
+#followed table
+class Followers(BaseModel):
     id: int
-    post_id : int
-    user_id : int
+    follower_uid: int
+    followed_uid : int
